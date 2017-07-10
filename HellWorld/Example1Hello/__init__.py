978ee993abde1bc688310252eea3f22a96c0c04f
Mustafa ERGAN <mustafaergan@gmail.com>
2017-07-10 19:43:37 +0300

    ptyhon baslangic

---
+++ HellWorld/Example1Hello/__init__.py
@@ -0,0 +1,3 @@
+print("hello world")
+print("First Python project")
+print("x is 1")
---
+++ HellWorld/Example2VariableType/__init__.py
@@ -0,0 +1,75 @@
+#int
+from test.pickletester import REX_five
+myint = 7
+print(myint)
+# float
+myfloat = 6.0
+print(myfloat)
+#float funk
+floatNumber = float(7)
+print(floatNumber)
+
+# float funk
+mystring = "i love there"
+print(mystring)
+
+#sum
+one = 1
+five = 5
+summ = one + five
+print(sum)
+
+#concat
+text1 = "Mustafa Kemal"
+text2 = " Ataturk"
+concat = text1 + text2
+print(concat)
+
+#farklı tiplere izin vermemektedir
+one = 1
+two = 2
+hello = "hello"
+#print(one + two + hello)
+
+#degersiz
+mystring = None
+print(mystring)
+
+#String tipler
+word = 'word'
+sentence = "This is a sentence."
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+print(word)
+print(sentence)
+print(paragraph)
+
+
+#coklu tanım
+a = b = c = 1
+
+print(a)
+print(b)
+print(c)
+
+a,b,c = 1,2,"john"
+
+print(a)
+print(b)
+print(c)
+
+#Buna izni yok
+#a=1,b=5
+
+#tanimlar 
+#int float complex
+
+number = int(5)
+print(number)
+number = float(5)
+print(number)
+number = complex(5)
+print(number)
+
+
+
