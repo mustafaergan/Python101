d77a33a40e1dce0b867beb142ef530ed5de59be6
Mustafa ERGAN <mustafaergan@gmail.com>
2017-07-13 19:33:11 +0300

    decision

---
+++ HellWorld/Example4BasicOperators/__init__.py
@@ -0,0 +1,177 @@
+##Python Arithmetic Operators
+
+# + add
+print("**add")
+a = 21
+b= 30
+c= a +b 
+print(c)
+# - sub
+print("**sub")
+a = 21
+b= 30
+c= a -b 
+print(c)
+# * multipcation
+print("**multipcation")
+a = 21
+b= 30
+c= a * b 
+print(c)
+# / division
+print("**division")
+a = 21
+b= 30
+c= a / b 
+print(c)
+# % modulus
+print("**multipcation")
+a = 41
+b= 30
+c= a % b 
+print(c)
+# ** Exponent(power) 
+print("**Exponent")
+a=2
+b=3
+c = a**b
+print(c)
+# // floor division
+print("**floor division")
+a=5
+b=3
+c = a//b
+print(c)
+a=5.0
+b=2.3
+c = a//b
+print(c)
+
+##Python Comparison Operators
+
+# == equal
+print("**equal")
+a = 21
+b = 10
+c = a == b
+print(c)
+
+a = 15
+b = 15
+c = a == b
+print(c)
+
+# != not equal
+print("**Not equal")
+a = 15
+b = 16
+c = a != b
+print(c)
+
+
+# > greater
+print("** > greater")
+a = 15
+b = 16
+c = (a > b)
+print(c)
+
+# > greater
+print("** < less")
+a = 15
+b = 16
+c = (a < b)
+print(c)
+
+# <= greater equals
+print("** <= less")
+a = 15
+b = 16
+c = (a <= b)
+print(c)
+
+# <= less equals
+print("** <= less")
+a = 15
+b = 16
+c = (a >= b)
+print(c)
+
+#Python Assignment Operators
+# = Assings values
+print("** = Assings values")
+a = 15
+print(a)
+
+# += Add and Assings values
+print("** += add and Assings values")
+a += 15
+print(a)
+
+# += Subtract and Assings values
+print("** = Subtract and Assings values")
+a -= 15
+print(a)
+
+# *= Multiplies and Assings values
+print("** *= Subtract and Assings values")
+a *= 3
+print(a)
+
+
+
+# *= Divide and Assings values
+print("** /= Divide and Assings values")
+a /= 2
+print(a)
+
+
+# %= Modulus and Assings values
+print("** %= Modulus and Assings values")
+a %= 3
+print(a)
+
+# **= Power and Assings values
+print("** **= Power and Assings values")
+a **= 2
+print(a)
+
+# //= Division Floor  and Assings values
+print("** //= Division Floor and Assings values")
+a //= 2
+print(a)
+
+# is - is not
+print("** is - is not")
+a = 15
+b = 16
+c = (a is b)
+print(c)
+
+a = 15
+b = 15
+c = (a is b)
+print(c)
+
+
+a = 15
+b = 15
+c = (a is not b)
+print(c)
+
+
+a = 15
+b = 16
+c = (a is not b)
+print(c)
+
+
+
+
+
+
+
+
+
+
+
