b3610f4e760b851b3ad674875363b57f80320981
Mustafa ERGAN <mustafaergan@gmail.com>
2017-07-13 19:33:48 +0300

    decision

---
+++ HellWorld/Example5Decision/__init__.py
@@ -0,0 +1,47 @@
+#if
+print("***")
+a = 5
+if (a == 5):
+    print("a equals 5")
+    print("in if")
+print("not if")
+print("***")
+a = 6
+if (a == 5):
+    print("a equals 6")
+    print("in if")
+print("not if")
+
+var1 = 100
+if var1:
+    print("values larger than zero or less than zero")
+    
+var1 = 0
+if var1:
+    print("values larger than zero or less than zero")
+    
+var1 = -1
+if var1:
+    print("values larger than zero or less than zero")    
+    
+    
+##if else
+veri = 80
+
+if(veri > 80):
+    print(" values larger than 80")
+elif(veri > 60):
+    print("values larger than 60") 
+elif(veri > 40):
+    print("values larger than 40")
+else:
+    print("values less than 40")  
+    
+veri = 110
+if(veri > 100):
+    if(veri < 120):
+        print("values larger than 100 and less than 120")  
+
+
+
+    
