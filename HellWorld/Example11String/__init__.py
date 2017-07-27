c8e7b2b9c6597e785e0c54265d82f21c4ff90e85
Mustafa ERGAN <mustafaergan@gmail.com>
2017-07-27 19:20:28 +0300

    String

--- HellWorld/Example11String/__init__.py
+++ HellWorld/Example11String/__init__.py
@@ -17,10 +17,36 @@ print(text+"\"Heyyy\"")
 print(text+"\'Heyyy\'")
 print(text+"\tHeyyy")
 
+print ('C:\\mustafa')
+print (r'C:\\nowhere')
+
 
 
 print("****Special*****")
 text1 = "Mustafa"
 text2 = "Ergan"
+
+print("+")
+print(text1+text2)
+
+print("*2")
+print(text1*2)
+print((text1 in 'Mustafa'))
+print((text1 not in 'u'))
+
+
+
+print("****Format*****")
+
+print ("My name is %s and weight is %d kg!" % (text1, 80)) 
+
+# %c character
+# %s string
+# %d decimal integer
+# %u unsigned decimal integer
+# %f floating point real number
+
+
+
 
 
